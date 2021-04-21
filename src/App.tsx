a40e668668ae9cec27248a25b37634877c136200
Elliana May <me@mause.me>
2021-04-21 18:35:58 +0800

    Add simple call

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useSWR from 'swr';
+import axios from 'axios';
 
 function App() {
+  const { data } = useSWR('https://mause-housing.builtwithdark.com/', key => axios.get(key, {responseType: 'json'}));
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Properties: { data?.data.length }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
