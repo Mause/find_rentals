3b0f2e98cd825ff263f2dce862982d21a17b315a
Elliana <me@mause.me>
2021-04-23 22:03:33 +0800

    Try to build status mapping

--- api/test.ts
+++ api/test.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import { GoogleSpreadsheet } from "google-spreadsheet";
+import { CellFormat, Color, GoogleSpreadsheet } from "google-spreadsheet";
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_ID!);
 doc.useApiKey(process.env.GOOGLE_API_KEY!);
@@ -7,12 +7,26 @@ doc.useApiKey(process.env.GOOGLE_API_KEY!);
 export default async (request: VercelRequest, response: VercelResponse) => {
   await doc.loadInfo();
   const sheet = doc.sheetsByIndex[0];
+
+  const statusMapping: {[key: string]: string} = {};
+
   const rows = (await sheet.getRows()).map(row => {
     const orow: any = {};
+
+    let cell = sheet.getCell(row.rowIndex, 0);
+    statusMapping[cell.backgroundColor.toString()] = sheet.headerValues[0];
+
     for (const header of sheet.headerValues.slice(1)) {
       orow[header] = row[header];
     }
+
     return orow;
   });
-  response.json({ title: doc.title, rows });
+
+  rows.forEach((row, idx) => {
+    let cell = sheet.getCell(row.rowIndex, idx + 1 + 1);
+    row.Status = statusMapping[cell.backgroundColor.toString()];
+  });
+
+  response.json({ title: doc.title, rows, statusMapping });
 };
