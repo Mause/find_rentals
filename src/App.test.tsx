80eabb06933a49e880a06f7e392824d6af74d826
Elliana May <me@mause.me>
2021-04-28 12:46:17 +0800

    Mend test assertion

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Find Rentals/i);
   expect(linkElement).toBeInTheDocument();
 });
