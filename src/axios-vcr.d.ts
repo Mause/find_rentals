246b924c24c9ef4e59f81ae8fbdd3d1732573d67
Elliana <me@mause.me>
2021-04-29 12:21:31 +0800

    Cleanup types

--- src/axios-vcr.d.ts
+++ src/axios-vcr.d.ts
@@ -1,4 +1,4 @@
-import { AxiosRequestConfig } from 'axios';
+import { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 declare module 'axios-vcr' {
   class RequestMiddleware {
@@ -9,33 +9,23 @@ declare module 'axios-vcr' {
           ) => AxiosRequestConfig | Promise<AxiosRequestConfig>)
         | undefined,
       failure: any
-    ) {
-      throw new Error('Method not implemented.');
-    }
+    ) {}
     static success(
       cassettePath: string
     ):
       | ((
           value: AxiosRequestConfig
         ) => AxiosRequestConfig | Promise<AxiosRequestConfig>)
-      | undefined {
-      throw new Error('Method not implemented.');
-    }
+      | undefined {}
   }
   class ResponseMiddleware {
     static success(
       cassettePath: string
     ):
       | ((
-          value: import('axios').AxiosResponse<any>
-        ) =>
-          | import('axios').AxiosResponse<any>
-          | Promise<import('axios').AxiosResponse<any>>)
-      | undefined {
-      throw new Error('Method not implemented.');
-    }
-    static failure(arg0: any, failure: any) {
-      throw new Error('Method not implemented.');
-    }
+          value: AxiosResponse<any>
+        ) => AxiosResponse<any> | Promise<AxiosResponse<any>>)
+      | undefined {}
+    static failure(arg0: any, failure: any) {}
   }
 }
