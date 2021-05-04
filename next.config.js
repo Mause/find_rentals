ab710846af1035fbb84239e890897504bb7f9d87
Elliana May <me@mause.me>
2021-05-04 10:07:16 +0800

    Fix release id

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-//Get the commit sha from either Github, Gitlab, or Bitbucket
-const COMMIT_SHA =
-  VERCEL_GITHUB_COMMIT_SHA ||
-  VERCEL_GITLAB_COMMIT_SHA ||
-  VERCEL_BITBUCKET_COMMIT_SHA;
-
 new SentryWebpackPlugin({
   include: '.next',
   ignore: ['node_modules'],
   urlPrefix: '~/_next',
-  release: COMMIT_SHA,
+  release: process.env.VERCEL_GIT_COMMIT_SHA,
 });
