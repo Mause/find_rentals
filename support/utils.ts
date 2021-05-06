150a9edd1ae95e3cb5460ef9f6ef56eb87d0bda3
Elliana <me@mause.me>
2021-05-06 15:33:24 +0800

    Add cached decorator

--- support/utils.ts
+++ support/utils.ts
@@ -1,13 +1,54 @@
 import { AxiosError } from 'axios';
+import { CollectionReference, Firestore } from '@google-cloud/firestore';
+import { logger } from './logger';
+
+type Toop<I, O> = TypedPropertyDescriptor<
+  (listingId: I) => Promise<O | undefined>
+>;
+
+const datastore = new Firestore({});
 
 function isAxiosError(error: any): error is AxiosError {
   return error.isAxiosError;
 }
 
+export function cached<I, O>(collectionName: string) {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: Toop<I, O>
+  ): void {
+    const method = descriptor.value!;
+    const collection = datastore.collection(
+      collectionName
+    ) as CollectionReference<O>;
+
+    descriptor.value = async function (listingId: I): Promise<O | undefined> {
+      const doc = collection.doc(
+        [propertyKey, 'listingId', listingId].join('/')
+      );
+
+      let data = (await doc.get()).data();
+
+      if (data) {
+        logger.info('cache hit');
+      } else {
+        logger.warn('cache miss', { listingId });
+        data = await method.call(this, listingId);
+        if (data) {
+          await doc.set(data);
+        }
+      }
+
+      return data;
+    };
+  };
+}
+
 export function handleError<I, O>(
   target: any,
   propertyKey: string,
-  descriptor: TypedPropertyDescriptor<(listingId: I) => Promise<O | undefined>>
+  descriptor: Toop<I, O>
 ): void {
   const method = descriptor.value!;
 
@@ -19,6 +60,11 @@ export function handleError<I, O>(
         switch (e.response?.status) {
           case 429:
           case 404:
+            logger.warn('call failed', {
+              listingId,
+              status: e.response.status,
+              data: e.response.data,
+            });
             return undefined;
         }
       }
