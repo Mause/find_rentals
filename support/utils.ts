b33a17e89293826c7a9637af96963d80c72cbc19
Elliana <me@mause.me>
2021-05-06 16:41:52 +0800

    Fix again

--- support/utils.ts
+++ support/utils.ts
@@ -10,10 +10,10 @@ type Toop<I, O> = TypedPropertyDescriptor<
 const keyFilename = 'find-rentals-c060658e2390.json';
 const serviceAccount = process.env.GCLOUD_CREDENTIALS
   ? JSON.parse(Buffer.from(process.env.GCLOUD_CREDENTIALS, 'base64').toString())
-  : {};
+  : undefined;
 const datastore = new Firestore({
   credentials: serviceAccount,
-  projectId: serviceAccount.project_id,
+  projectId: serviceAccount?.project_id,
   keyFilename: serviceAccount ? undefined : keyFilename,
 });
 
