f13dda54901c63cb9776232f07dbcfe38107b344
Elliana May <me@mause.me>
2023-10-02 17:53:35 +0800

    fix: frontload string conversion

--- api/inspections.ts
+++ api/inspections.ts
@@ -2,7 +2,7 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import { uk } from 'chrono-node';
 import { getProperties } from './data';
 import { augment } from '../support';
-import { compareAsc, endOfYesterday, startOfDay } from 'date-fns';
+import { startOfDay } from 'date-fns';
 import _ from 'lodash';
 
 export default async function (
@@ -15,8 +15,6 @@ export default async function (
 async function getData() {
   const { rows, statusMapping } = await getProperties();
 
-  const yesterday = endOfYesterday();
-
   const today = rows
     .filter((prop) => prop['Viewed?'])
     .filter((prop) => prop['Viewed?']!.toLowerCase() !== 'requested')
@@ -24,13 +22,12 @@ async function getData() {
       prop,
       viewed: startOfDay(uk.parseDate(prop['Viewed?']!)),
     }));
-  // .filter(({ viewed }) => compareAsc(viewed, yesterday));
 
   await augment(today.map(({ prop }) => prop));
 
   return {
     today: _.sortBy(
-      Object.entries(_.groupBy(today, (row) => row.viewed)),
+      Object.entries(_.groupBy(today, (row) => row.viewed.toISOString())),
       ([date]) => date
     ),
     statusMapping,
