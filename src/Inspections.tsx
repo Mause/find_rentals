fcc40d11ab09eff76753d68f74a1bb40762cddf2
Elliana May <me@mause.me>
2023-10-02 17:30:29 +0800

    feat: pagination

--- src/Inspections.tsx
+++ src/Inspections.tsx
@@ -1,19 +1,34 @@
 import useSWR from 'swr';
 import * as types from './types';
+import { ReturnShape } from '../api/inspections';
+import { useState } from 'react';
+import { Button, Pagination } from 'react-bulma-components';
 
 export default function Inspections() {
-  const { data, isValidating, error } = useSWR('/api/inspections');
-  const today: types.Property[] = data?.today || [];
+  const { data, isValidating, error } = useSWR<ReturnShape>('/api/inspections');
+  const days = Object.entries(data?.today || {});
+
+  const [selectedDay, setSelectedDay] = useState(0);
+
+  const [date, day] = days[selectedDay];
 
   return (
-    <ul>
-      {error ? <li>{error.toString()}</li> : undefined}
-      <li> {isValidating ? 'Loading....' : 'Ready'}</li>
-      {today.map((prop: any) => (
-        <li key={prop.Address}>
-          {prop.Address} ~ {prop['Viewed?']}
-        </li>
-      ))}
-    </ul>
+    <>
+      <h3>{date}</h3>
+      <Pagination
+        current={selectedDay}
+        total={days.length}
+        onChange={setSelectedDay}
+      />
+      <ul>
+        {error ? <li>{error.toString()}</li> : undefined}
+        <li> {isValidating ? 'Loading....' : 'Ready'}</li>
+        {day.map((prop: any) => (
+          <li key={prop.Address}>
+            {prop.Address} ~ {prop['Viewed?']}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
